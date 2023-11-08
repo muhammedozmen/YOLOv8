679db4d9761a20d642ddcd2f840ae9c006737083
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-11-08 14:58:54 +0100

    virtual env changed

--- yolov8.py
+++ yolov8.py
@@ -5,11 +5,11 @@ from PIL import Image
 model = YOLO('yolov8n.pt')
 
 # object recognition in image
-im1 = Image.open("Elon_Musk.jpg")
-im1_result = model.predict(source=im1, save=True)
+#im1 = Image.open("Elon_Musk.jpg")
+#im1_result = model.predict(source=im1, save=True)
 
 # object recognition in live cam
-#cam_result = model.predict(source="0", show=True)
+cam_result = model.predict(source="0", show=True)
 
 # object recognition in video
-video_result = model.predict(source="video.mp4", show=True)
+#video_result = model.predict(source="video.mp4", show=True)
